1dc79506fe44d771e650243eb48dfb81e52396cf
andritoky <randriamampionona9@gmail.com>
2022-06-23 17:50:25 +0200

    test validation

--- src/component/home/form/form.test.tsx
+++ src/component/home/form/form.test.tsx
@@ -2,6 +2,7 @@ import { render, cleanup } from "@testing-library/react"
 import { FormHome } from "./form"
 import ReactDOM from 'react-dom/client';
 import { screen } from "@testing-library/dom"
+import { validate } from "./validation";
 
 afterEach(cleanup)
 
@@ -17,7 +18,17 @@ test("render FormHome correctly", () => {
 
 //Cherche si y a un text "Numero de telephone*" dans component
 test("render FormHome correctly 2", () => {
-    const { getByTestId } = render(<FormHome />);
+    render(<FormHome />);
     const label = screen.getByText("Numero de telephone*")
     expect(label).toBeInTheDocument()
 })
+
+test("validation  ", () => {
+    const data = { numero: "", nom: "", prenom: "", email: "", codePostal: "625" };
+    const data2 = { numero: "52", nom: "", prenom: "", email: "", codePostal: "" };
+    const data3 = { numero: "52526", nom: "", prenom: "", email: "", codePostal: "" };
+    expect(Object.keys(validate(data)).length).toBe(4)
+    expect(Object.keys(validate(data2)).length).toBe(5)
+    expect(Object.keys(validate(data3)).length).toBe(4)
+})
+
